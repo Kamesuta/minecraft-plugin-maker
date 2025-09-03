06a175c920e18c45760de713d2b834951f2bbdb0
Kamesuta <kamesuta@gmail.com>
2025-09-03 17:24:03 +0000

    fix: 権限周りの修正

--- scripts/setup_devcontainer.sh
+++ scripts/setup_devcontainer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# scriptsフォルダ内のすべてのスクリプトに実行権限を付与
+echo "★ scriptsフォルダ内のスクリプトに実行権限を付与しています..."
+chmod +x scripts/*
+
 # ヘッドレスJava GUIの依存関係(マイクラサーバー起動に必要)をインストール
 echo "★ マイクラサーバーの起動に必要なパッケージをインストールしています..."
 sudo apt-get update
