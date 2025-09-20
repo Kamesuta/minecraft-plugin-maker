83c1de29b2f7ed91460a1031c9a60749c0b6e700
Kamesuta <kamesuta@gmail.com>
2025-09-20 11:22:05 +0000

    refactor: rcon.sh の引数が""で囲われていなくてもOK

--- scripts/rcon.sh
+++ scripts/rcon.sh
@@ -12,23 +12,22 @@ MCRCON_PATH="$(dirname "$0")/mcrcon"
 
 # 引数が指定されていない場合、スクリプトの正しい使い方を表示して終了
 if [ $# -eq 0 ]; then
-  echo "使い方: $0 \"コマンド1\" \"コマンド2\" ..."
+  echo "使い方: $(basename "$0") <コマンド>"
   echo "MinecraftサーバーにRCON経由でコマンドを送信します。"
   exit 1
 fi
 
 # 送信するコマンドのバリデーションを行う
 # 特定のコマンド（reload, stop）は、意図しないサーバーの挙動を防ぐために禁止
-for cmd in "$@"; do
-  if [[ "$cmd" == "reload" || "$cmd" == "reload confirm" ]]; then
-    echo "Error: 'reload' or 'reload confirm' commands are not allowed. Use 'plugman reload <PluginName>' instead."
-    exit 1
-  fi
-  if [[ "$cmd" == "stop" ]]; then
-    echo "Error: 'stop' command is not allowed. To restart the server, you need to manually type 'stop' and then press F5."
-    exit 1
-  fi
-done
+cmd="$*"
+if [[ "$cmd" == "reload" || "$cmd" == "reload confirm" ]]; then
+  echo "Error: 'reload' or 'reload confirm' commands are not allowed. Use 'plugman reload <PluginName>' instead."
+  exit 1
+fi
+if [[ "$cmd" == "stop" ]]; then
+  echo "Error: 'stop' command is not allowed. To restart the server, you need to manually type 'stop' and then press F5."
+  exit 1
+fi
 
 # mcrconを実行
-"$MCRCON_PATH" -p "$RCON_PASSWORD" "$@"
+"$MCRCON_PATH" -p "$RCON_PASSWORD" "$*"
